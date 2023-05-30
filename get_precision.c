2a73c70bbf9e755a2c9d1e3a071d3961e68c4d0d
Rachael Yusuf <rachaelroy11@gmail.com>
2023-05-30 15:31:41 -0700

    updated

--- get_precision.c
+++ get_precision.c
@@ -13,12 +13,12 @@ int get_precision(const char *format, int *i, va_list list)
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_1] != '.')
+	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0' curr_i++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
@@ -38,3 +38,4 @@ int get_precision(const char *format, int *i, va_list list)
 	*i = curr_i - 1;
 
 	return (precision);
+}
